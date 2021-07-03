01e6943fc513025a26e77e193aac97a415935463
mohamedisakr <mohamedisakr@gmail.com>
2021-07-03 10:52:44 +0200

    implementing challenge 1 to 5

--- async-javascript/pair-prog.js
+++ async-javascript/pair-prog.js
@@ -17,45 +17,80 @@ function testMe() {
   console.log("Partnah");
 }
 
-testMe();
+// testMe();
 // After thinking it through, uncomment the following line to check your guess!
 // testMe(); // what order should these log out? Howdy or Partnah first?
+
+//=========================================================================================//
 
 /* CHALLENGE 2 */
 // Create a function delayedGreet that console logs welcome after 3 seconds.
 function delayedGreet() {
   // ADD CODE HERE
+  setTimeout(() => {
+    console.log("welcome");
+  }, 3000);
 }
+
+// delayedGreet();
+
 // Uncomment the following line to check your work!
 // delayedGreet(); // should log (after 3 seconds): welcome
+
+//=========================================================================================//
 
 /* CHALLENGE 3 */
 // Create a function helloGoodbye that console logs hello right away, and good bye after
 // 2 seconds.
 function helloGoodbye() {
   // ADD CODE HERE
+  console.log("hello");
+  setTimeout(() => {
+    console.log("good bye");
+  }, 2000);
 }
+
+// helloGoodbye();
+
 // Uncomment the following line to check your work!
 // helloGoodbye(); // should log: hello // should also log (after 3 seconds): good bye
+
+//=========================================================================================//
 
 /* CHALLENGE 4 */
 // Create a function brokenRecord that console logs hi again every second.
 // Use the End Code button to stop the console logs when you are satisfied that it is working.
 function brokenRecord() {
   // ADD CODE HERE
+  setInterval(() => {
+    console.log("hi again");
+  }, 1000);
 }
+
+// brokenRecord();
+
 // Uncomment the following line to check your work!
-// brokenRecord(); // should log (every second): hi again
+// should log (every second): hi again
+
+//=========================================================================================//
 
 /* CHALLENGE 5 */
 // Create a function limitedRepeat that console logs hi for now every second,
 // but only for 5 seconds. Research how to use clearInterval if you are not
 // sure how to do this.
 function limitedRepeat() {
-  // ADD CODE HERE
+  const callback = () => {
+    console.log("hi");
+  };
+  setTimeout(clearInterval, 5000, setInterval(callback, 1000));
 }
+
+// limitedRepeat();
+
 // Uncomment the following line to check your work!
 // limitedRepeat(); // should log (every second, for 5 seconds): hi for now
+
+//=========================================================================================//
 
 /* CHALLENGE 6 */
 /*
